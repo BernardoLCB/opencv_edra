2217c0001ef3cd65afddb2dd5653da012b4fe57a
BernardoLCB <bernardoleinig@gmail.com>
2025-03-15 16:17:26 -0300

    Update main.py

--- main.py
+++ main.py
@@ -23,9 +23,9 @@ cv2.createTrackbar("Matrix.Num", "CONTROL", 0, 50, nothing)
 #cv2.createTrackbar("Brightness", "CONTROL", 0, 255, nothing)
 cv2.createTrackbar("Threshold", "CONTROL", 0, 255, nothing)
 cv2.createTrackbar("Color", "CONTROL", 0, 2, nothing)
-cv2.createTrackbar("Area.Cross", "CONTROL", 0, 5000, nothing)
-cv2.createTrackbar("Area.Square", "CONTROL", 0, 5000, nothing)
-cv2.createTrackbar("Area.Circle", "CONTROL", 0, 5000, nothing)
+cv2.createTrackbar("Ar.Cross", "CONTROL", 2000, 10000, nothing)
+cv2.createTrackbar("Ar.Square", "CONTROL", 2000, 10000, nothing)
+cv2.createTrackbar("Ar.Circle", "CONTROL", 2000, 10000, nothing)
 
 
 #=====================================================================#
@@ -42,11 +42,16 @@ while (True):
     #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/inputs/chosen/IMG_20231201_133104323_jpg.rf.66b05d13d8eaa073f17c4c6d7ae07117.jpg") # nao é o melhor valor 1 2 2 255 42 1
     #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/inputs/chosen/20220930_103000-heic_jpg.rf.b44a35e7ecbd299bcb68e3f4989eaf4f.jpg")    # ainda nao tem 
     #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/inputs/Base_de_Takeoff.png")                                                          # 0 0 0 255 0 2 0 0 0
-    #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/inputs/chosen/20220930_100330-heic_jpg.rf.cd762474c8ba772d242008004c2e1333.jpg") # 0 5 5 255 145 1 0 0 0 / 4 4 5 0 2 / 0 5 2 40 1 0 627 0
+    sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/inputs/chosen/20220930_100330-heic_jpg.rf.cd762474c8ba772d242008004c2e1333.jpg") # 0 5 5 255 145 1 0 0 0 / 4 4 5 0 2 / 0 5 2 40 1 0 627 0
     #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/inputs/chosen/20220930_103031-heic_jpg.rf.64a4ed37cb175342d604fb8e89b8c498.jpg") # preciso alterar o valor do filtro ante-reflexo
     #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/inputs/chosen/20220930_104918-heic_jpg.rf.45c0bd59a930f86991c7d35894fdf8e7.jpg")
     #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/inputs/meus_inputs/7.new.jpg") # 3 0 0 87 2
-    sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/inputs/meus_inputs/8.jpg") # 3 0 0 87 2
+    #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/inputs/meus_inputs/8.jpg") # 3 0 0 87 2 hard
+    #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/novos_inputs/ex01.jpg") # 1 0 0 14 2 2000 2000 2000
+    #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/novos_inputs/ex02.jpg") # 2 0 0 38 2 2000 2000 2000
+    #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/novos_inputs/ex03.jpg") # 0 0 0 64 2 2000 2000 2000
+    #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/novos_inputs/ex07.jpg") # nao dei
+    #sorce_imagem = cv2.imread("C:/Users/adm/Documents/EDRA-OPENCV/novos_inputs/ex08.jpg") # 3 4 8 0 2 2000 2000 2000
     #sorce_imagem = cv2.resize(sorce_imagem,(640, 640))
     if sorce_imagem is None:
         sys.exit("ERROR: COULD NOT READ THE IMAGE")
@@ -63,9 +68,9 @@ while (True):
     #sliders4 = cv2.getTrackbarPos("Brightness", "CONTROL")          # resposável por aplicar brilho a imagem
     sliders5 = cv2.getTrackbarPos("Threshold", "CONTROL")           # resposável por aplicar o limiar na imagem
     sliders6 = cv2.getTrackbarPos("Color", "CONTROL")               # resposável por fazer com que somente a cor amarela ou azul seja identificada na imagem
-    sliders7 = cv2.getTrackbarPos("Area.Cross", "CONTROL")          # resposável por exibir as formas geométricas de acordo com a área definida
-    sliders8 = cv2.getTrackbarPos("Area.Square", "CONTROL")
-    sliders9 = cv2.getTrackbarPos("Area.Circle", "CONTROL")
+    sliders7 = cv2.getTrackbarPos("Ar.Cross", "CONTROL")          # resposável por exibir as formas geométricas de acordo com a área definida
+    sliders8 = cv2.getTrackbarPos("Ar.Square", "CONTROL")
+    sliders9 = cv2.getTrackbarPos("Ar.Circle", "CONTROL")
     #--------------------------------------------------------------------------------------------------------#
 
 
@@ -88,7 +93,7 @@ while (True):
     #------------------------------------------------------------------------------------------------------------------------#
 
     #----------------------ENCONTRANDO OS CONTORNOS DA IMAGEM QUE FOI BINARIZADA LOGO ACIMA----------------------
-    contours,_ = cv2.findContours(binarized_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    contours,hierarquia = cv2.findContours(binarized_image, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
     #cv2.drawContours(sorce_imagem, contours, -1, (0, 255, 0),2)
 
     #(contours, sorce_imagem, sliders7, sliders8, sliders9)
@@ -96,7 +101,7 @@ while (True):
     # print(f"numero de dimensoes antes gray image copy--> {gray_image_copy.shape}")
     gray_image_copy = np.stack((gray_image_copy,)*3, axis=-1)
     # print(f"numero de dimensoes depois gray image copy--> {gray_image_copy.shape}")
-    findShapes(contours, gray_image_copy, sliders7, sliders8, sliders9)
+    findShapes(contours, gray_image_copy, hierarquia, sliders7, sliders8, sliders9)
     
     # print(f"numero de dimensoes antes gray image --> {gray_image.shape}")
     # print(f"numero de dimensoes antes binarized_image --> {binarized_image.shape}")
@@ -108,4 +113,6 @@ while (True):
 
     cv2.imshow("IMAGES", img_stack)
     
-    cv2.waitKey(300)
+    #print("passei")
+    cv2.waitKey(1000) #300
+    print("oi")
